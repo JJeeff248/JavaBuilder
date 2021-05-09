3d022898f85134b7d4d669aa2e535cf5bd15ac09
JJeeff248 <52386683+JJeeff248@users.noreply.github.com>
2021-05-09 13:10:34 +1200

    Update JavaBuilder.py

--- JavaBuilder/JavaBuilder.py
+++ JavaBuilder/JavaBuilder.py
@@ -1,7 +1,7 @@
 # JavaBuilder.py
 # Compile and run java files from sublime in the command prompt
 
-# Modified: 18/01/2020
+# Modified: 05/03/2021
 # Created: 15/01/2020
 # By JJeeff248
 
@@ -12,14 +12,13 @@ class JavabuilderCommand(sublime_plugin.TextCommand):
         for view in self.view.window().views():
             view.run_command('save')
 
-        def get_path(self):
+        def get_path(self): 
             """Get the current file path"""
             file_path = self.view.file_name() # C:\users\..\file.java
             path = file_path.split("\\")
             file_name = get_file(path)
             path.pop()
-            startup = directory_cmd(path)
-            create_bat(startup, file_name)
+            return path, file_name
             
         def get_file(path):
             """Gets the name of the file without extension"""
@@ -29,22 +28,33 @@ class JavabuilderCommand(sublime_plugin.TextCommand):
             return file_name
 
         def directory_cmd(path):
-            """Create directory command"""
+            """Create the change directory command"""
             current_driver = path[0]
-            current_directory = "\\".join(path)
-            startup = "cd " + current_directory + " & " + current_driver
-            return startup
-            
-        def create_bat(startup, file_name):
-            """Create batch file"""
+            current_directory = "/".join(path)
+            change_directory = "cd " + current_directory + " & " + current_driver
+            return change_directory, current_driver
+
+        def create_bat(change_directory, file_name):
+            """Creates a batch file containing the commands to compile and run the Java file"""
             commands = open(os.path.dirname(__file__) + '/../commands.bat', "w+")
-            commands.write(startup+"\njavac "+file_name+".java \ncls\njava "+file_name+"\npause")
-            commands.close()
+            commands.write("@echo off\n"
+                            + change_directory
+                            + "\njavac " + file_name + ".java"
+                            + "\nstart cmd /k java " + file_name
+                            )
+            commands.close() 
 
         def run_bat():
+            """Runs the batch file that was created"""
             batchfile = os.path.dirname(__file__) + '/../commands.bat'
             subprocess.call([r'{}'.format(batchfile)])
             os.remove(os.path.dirname(__file__) + '/../commands.bat')
 
-        get_path(self)
-        run_bat()
+        def main(self):
+            """Main routine"""
+            path, file_name = get_path(self)
+            change_directory, current_driver = directory_cmd(path)
+            create_bat(change_directory, file_name)
+            run_bat()
+            
+        main(self)
